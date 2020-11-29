ec72a2ad70e76b2407a61ed7d9cdbe83fb8d7b2e
Jessica Sewell <jessica@Jessicas-MacBook-Pro.local>
2020-11-29 11:51:04 -0800

    added example config

---
+++ config_example.py
@@ -0,0 +1,17 @@
+# Lob API keys
+live_secret = ''
+test_secret = ''
+
+# Manager
+mgr_usr = ''
+mgr_pass = ''
+mgr_url = '' # This is the manager API url. Example: 'https://{your_domain}.manager.io/api/{company_key}'
+
+
+documents = [
+	{
+		'name': 'Lease',
+		'id': '1'
+	}]
+
+
