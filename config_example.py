9403d6ea5dc1fa7320bd6f58140d48e453cc7377
Jessica Sewell <jessica@Jessicas-MacBook-Pro.local>
2020-11-29 15:09:15 -0800

    updated config

--- config_example.py
+++ config_example.py
@@ -5,7 +5,26 @@ test_secret = ''
 # Manager
 mgr_usr = ''
 mgr_pass = ''
-mgr_url = '' # This is the manager API url. Example: 'https://{your_domain}.manager.io/api/{company_key}'
+mgr_domain = '' # this is your manager subdomain
+mgr_api_root = '' # this is the url string after ...manager.io/api/
+mgr_url = f'https://{mgr_domain}.manager.io/api/{mgr_api_root}/'
+employee_url = '' # the url string for employee index
+bank_url = '' # the url string for bank account index
+invoice_url = '' # the url string for invoice index
+payslip_url = '' # url string for payslips, not the index.json
+
+# Manager payroll account items. You will need to create payslip items under settings in Manager and then get the account id's from the api
+earnings = ''
+federal_deduction = ''
+medicare_deduction = ''
+ss_deduction = ''
+employer_medicare = ''
+employer_ss = ''
+
+
+# Manager custom fields - input the field ID for each 
+pay_rate = ''
+fed_holding = ''
 
 
 documents = [
